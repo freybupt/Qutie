9bb224a7d0e22a2753fae76f8372e6457856f859
Liang Shi <freybupt@d142-058-228-180.wireless.sfu.ca>
2011-11-19 15:35:58 -0800

    Qutie v1.0

--- iphone/ViewController.h
+++ iphone/ViewController.h
@@ -7,7 +7,32 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ReaderOverlayView.h"
+#import "ZBarReaderViewController.h"
+#import <AVFoundation/AVAudioPlayer.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <ZBarReaderDelegate> {
+    
+    NSMutableArray *listItems;
+    ZBarReaderViewController *reader;
+    ReaderOverlayView *overlay;
+    AVAudioPlayer *beep;
+    UITableView *tableView;
+    
+    IBOutlet UIWebView *webView;
+    
+//    IBOutlet UIWindow *window;
+//    IBOutlet UINavigationController *navigationController;
+}
+
+- (IBAction)showInfo:(id)sender;
+@property (nonatomic, retain) NSMutableArray *listItems;
+@property (nonatomic, retain) ZBarReaderViewController *reader;
+@property (nonatomic, retain) ReaderOverlayView *overlay;
+@property (nonatomic, retain) UIWebView *webView;
+
+//@property (nonatomic, retain) IBOutlet UIWindow *window;
+//@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+- (void)initReader:(NSInteger)src;
 
 @end
--- iphone/ViewController.m
+++ iphone/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <CommonCrypto/CommonDigest.h>
+#import "WebViewController.h"
 
 @implementation ViewController
+
+@synthesize listItems, reader, overlay, webView;
+
+//@synthesize window, navigationController;
 
 - (void)didReceiveMemoryWarning
 {
@@ -50,6 +56,223 @@
 {
 	[super viewDidDisappear:animated];
 }
+
+#pragma mark -
+- (void) initReader: (NSInteger) src
+{
+    
+    if (src == UIImagePickerControllerSourceTypeCamera) {
+        reader = [ZBarReaderViewController new];
+        
+        // use a custom overlay
+        reader.showsZBarControls = NO;
+        overlay = [ReaderOverlayView new];
+//        overlay.delegate = self;
+    } else {
+        reader = (id)[ZBarReaderController new];
+    }
+    
+    reader.readerDelegate = self;
+    
+    ZBarImageScanner *scanner = reader.scanner;
+    
+    // show EAN variants as such
+    [scanner setSymbology: ZBAR_UPCA
+                   config: ZBAR_CFG_ENABLE
+                       to: 1];
+    [scanner setSymbology: ZBAR_UPCE
+                   config: ZBAR_CFG_ENABLE
+                       to: 1];
+    [scanner setSymbology: ZBAR_ISBN13
+                   config: ZBAR_CFG_ENABLE
+                       to: 1];
+    
+    // disable rarely used i2/5 to improve performance
+    [scanner setSymbology: ZBAR_I25
+                   config: ZBAR_CFG_ENABLE
+                       to: 0];
+}
+
+#pragma mark -
+#pragma mark ReaderOverlayDelegate
+- (void)readerOverlayDidDismiss {
+    //   [overlay willDisappear];
+    [reader dismissModalViewControllerAnimated: YES];
+}
+
+#pragma mark -
+#pragma mark ZBarReaderDelegate
+- (void)imagePickerController: (UIImagePickerController*) picker didFinishPickingMediaWithInfo: (NSDictionary*) info {
+    UIImage *image = [info objectForKey: UIImagePickerControllerOriginalImage];
+    
+    id <NSFastEnumeration> results = [info objectForKey: ZBarReaderControllerResults];
+    ZBarSymbol *sym = nil;
+    for(sym in results)
+        break;
+    assert(sym);
+    assert(image);
+    if(!sym || !image)
+        return;
+    
+    //  NSLog(@"type = %@", sym.type);
+    NSLog(@"data = %@", sym.data);
+    NSMutableString *outputString = [[NSMutableString alloc] init];
+    [outputString appendString:@"<html><head><style>body,a {font-family: helvetica,arial,sans-serif; font-size:16px; line-height:1.6em; background-color:#000; color: #fdba31; text-decoration: none;}</style></head><body>"];
+    
+    
+    NSArray *s = [sym.data componentsSeparatedByString:@"\n"];
+    for (NSString *str in s) {
+        NSLog(@"%@", str);
+        if ([str length] >0) {
+            
+            [outputString appendFormat:@"%@<BR>",str];
+        }
+    }
+    
+    [outputString appendString:@"</body></html>"];
+    
+    NSString *resultString = sym.data;
+    
+    [picker dismissModalViewControllerAnimated: YES];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Got it!" message:resultString delegate:self cancelButtonTitle:nil otherButtonTitles:@"Feed it!", nil];
+    [alert show];
+	
+//    resultString = @"http://209.87.59.177/w/qutie/";
+	//Create a URL object from scaned link
+	NSURL *url = [NSURL URLWithString:resultString];
+	
+	//URL Requst Object
+	NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];     
+	
+	//Load the request in the UIWebView.
+//	[webView loadRequest:requestObj];
+    
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"]isDirectory:NO]]];
+    
+    [self performSelector: @selector(playBeep)
+                   withObject: nil
+                afterDelay: 0.005];
+    //    WebViewController *wvTutorial = [[WebViewController alloc] initWithNibName:@"WebView" bundle:[NSBundle mainBundle]];
+    //	
+//    [self.navigationController pushViewController:wvTutorial animated:NO];
+    
+    //    WebViewController *aWebViewController = [[WebViewController alloc] initWithNibName:@"WebViewController" bundle:nil];
+    //	aWebViewController.title = @"QR Code Link";
+    //
+    ////    NSString *linkUrl = [NSString stringWithFormat:@"http://%@", thisQRCode.data];
+    //    NSString *headerUrl = [thisQRCode.data substringToIndex:4];
+    //    if([headerUrl isEqualToString:@"http"]){
+    //        aWebViewController.loadURL = [NSURL URLWithString:thisQRCode.data];
+    //        [self.navigationController pushViewController:aWebViewController animated:YES];
+    //        [aWebViewController release];
+    //    }
+    //    if(picker.sourceType == UIImagePickerControllerSourceTypeCamera) {
+    //        if (!TARGET_IPHONE_SIMULATOR) {
+    //            ZBarReaderViewController *_reader = (id)picker;
+    //            _reader.readerView.captureReader.enableReader = NO;
+    //        }
+    //        
+    //        [self performSelector: @selector(playBeep)
+    //                   withObject: nil
+    //                   afterDelay: 0.005];
+    //    }
+    
+}
+
+
+- (void) initAudio {
+    if(beep)
+        return;
+    NSError *error = nil;
+    beep = [[AVAudioPlayer alloc]
+            initWithContentsOfURL:
+            [[NSBundle mainBundle]
+             URLForResource: @"scan"
+             withExtension: @"wav"]
+            error: &error];
+    if(!beep)
+        NSLog(@"ERROR loading sound: %@: %@",
+              [error localizedDescription],
+              [error localizedFailureReason]);
+    else {
+        beep.volume = .5f;
+        [beep prepareToPlay];
+    }
+}
+
+- (void) playBeep {
+    if(!beep)
+        [self initAudio];
+    [beep play];
+}
+
+- (NSString*)md5HexDigest:(NSString*)input {
+    const char* str = [input UTF8String];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(str, strlen(str), result);
+    
+    NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH*2];
+    for(int i = 0; i<CC_MD5_DIGEST_LENGTH; i++) {
+        [ret appendFormat:@"%02x",result[i]];
+    }
+    return ret;
+}
+
+
+- (IBAction)showInfo:(id)sender
+{
+    
+    // test plain image picker
+    //    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    // //   imagePicker.delegate = self;
+    //    imagePicker.allowsEditing = NO;
+    //    imagePicker.showsCameraControls = NO;
+    //    imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    //    [self presentModalViewController:imagePicker animated:YES];
+    
+    
+    NSInteger src = UIImagePickerControllerSourceTypeCamera;
+    
+    if(!reader || (reader.sourceType != src))
+        [self initReader: src];
+    
+    if([UIImagePickerController isSourceTypeAvailable: src]) {
+        reader.sourceType = src;
+        
+        if(src == UIImagePickerControllerSourceTypeCamera) {
+            reader.showsCameraControls = NO;
+            [overlay setMode: OVERLAY_MODE_CANCEL];
+            reader.cameraOverlayView = overlay;
+            [overlay willAppear];
+        }
+        
+        [self presentModalViewController:reader animated:NO];
+    }
+    else {
+        NSString *title, *message;
+        if(src == UIImagePickerControllerSourceTypeCamera) {
+            title = @"Camera Unavailable";
+            message = @"Unable to access the camera";
+        }
+        else {
+            title = @"Image Library Unavailable";
+            message = @"Unable to access the image library";
+        }
+        UIAlertView *alert =
+        [[UIAlertView alloc]
+         initWithTitle: title
+         message: message
+         delegate: nil
+         cancelButtonTitle: @"Cancel"
+         otherButtonTitles: nil];
+        [alert show];
+    }
+    
+    
+}
+
+
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
